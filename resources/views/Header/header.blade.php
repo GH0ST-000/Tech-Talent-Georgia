e1e4c94c045493a6feccbd0e5d9271ffdee89aa1
Mac <mac@MacBook-Pro.local>
2021-12-28 12:24:16 +0400

    Header

---
+++ resources/views/Header/header.blade.php
@@ -0,0 +1,51 @@
+<div class="flex justify-between items-center px-4 py-6 bg-white border">
+<div class="flex justify-between items-center px-24 py-4">
+    <div class="flex justify-between items-center">
+    <img src="{{asset('image/logo.png')}}" class="rounded-full w-12 h-12">
+    <span class="font-mono px-3 text-gray-600 px-2 hover:text-black"><b class="text-black">Tech</b> Employee</span>
+    </div>
+    <div class="flex justify-between items-center text-center px-4">
+        <div class="w-12 h-12 bg-indigo-300 rounded-full justify-center flex">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="hover:bg-indigo-500 hover:text-white bi bi-journal-check w-8 h-8 rounded-full mt-2 text-black bg-white" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M10.854 6.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 8.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+            </svg>
+        </div>
+        <a href="#" class="font-mono text-gray-600 px-2 hover:text-black">Jobs</a>
+    </div>
+    <div class="flex justify-between items-center text-center px-5">
+        <div class="w-12 h-12 bg-indigo-300 rounded-full justify-center flex">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class=" hover:bg-indigo-500 hover:text-white bi bi-compass w-8 h-8 rounded-full mt-2 text-black bg-white" viewBox="0 0 16 16">
+                <path d="m6.94 7.44 4.95-2.83-2.83 4.95-4.949 2.83 2.828-4.95z"/>
+            </svg>
+        </div>
+        <a href="#" class="font-mono px-2 text-gray-600 px-2 hover:text-black">Courses</a>
+    </div>
+    <div class="flex justify-between items-center text-center px-5 ">
+        <div class="w-12 h-12 bg-indigo-300 rounded-full justify-center flex">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-square w-8 h-8 rounded-full mt-2 text-black bg-white hover:bg-indigo-500 hover:text-white" viewBox="0 0 16 16">
+          <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z"/>
+            </svg>
+        </div>
+        <a href="#" class="font-mono px-2 text-gray-600 px-2 hover:text-black">Events</a>
+    </div>
+    <div class="flex justify-between items-center text-center px-5">
+        <div class="w-12 h-12 bg-indigo-300 rounded-full justify-center flex">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="hover:bg-indigo-500 hover:text-white bi bi-calendar2-check w-8 h-8 rounded-full mt-2 text-black bg-white" viewBox="0 0 16 16">
+                <path d="M10.854 8.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L7.5 10.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+                <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z"/>
+            </svg>
+        </div>
+        <a href="#" class="font-mono px-2 text-gray-600 px-2 hover:text-black">Content</a>
+    </div>
+</div>
+
+<div class="flex justify-center items-center text-center">
+    <button class="w-24 border px-3 py-3 mr-4 rounded-lg font-mono shadow focus:border-indigo-700 hover:border-indigo-800">Login</button>
+    <button class="w-24 border px-3 py-3 rounded-lg font-mono bg-indigo-500 text-white mr-10 shadow focus:bg-indigo-400 hover:border-indigo-800">Regiter</button>
+
+</div>
+</div>
+
+</div>
+
+
---
+++ resources/views/main/main.blade.php
@@ -0,0 +1,7 @@
+<div class="w-full border bg-indigo-500" style="height: 400px">
+<div class="flex items-center justify-center text-center">
+    <div class="images items-center justify-center flex bg-image">
+
+    </div>
+</div>
+</div>
